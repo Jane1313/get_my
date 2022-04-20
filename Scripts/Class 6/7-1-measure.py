9229a85169d7a1759b70e6e5fb0cf22eff1e38b6
Jane <miniyanochka13@gmail.com>
2022-04-20 12:09:00 +0300

    class 7 not complited

--- Scripts/Class 6/7-1-measure.py
+++ Scripts/Class 6/7-1-measure.py
@@ -1,4 +1,26 @@
 import matplotlib.pyplot as plt
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+dac = [26, 19, 13, 6, 5, 11, 9, 10]
+comp = 4
+troyka = 17
+leds = [21,20,16,12,7,8,25,24]
+
+GPIO.setup(dac, GPIO.OUT)
+GPIO.setup(troyka, GPIO.OUT)
+GPIO.setup(comp, GPIO.IN)
+GPIO.setup(leds, GPIO.OUT)
+
+
+
+
+
+
+
+
 
 measured_data = [10, 23, 45, 50, 100, 155, 204, 255]
 plt.plot (measured_data)
