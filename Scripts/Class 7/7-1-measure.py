5614bd448be6e4d9f0c5233303503d3f9a45a338
Jane <miniyanochka13@gmail.com>
2022-05-03 13:44:16 +0300

    сделано 7 задание

--- Scripts/Class 7/7-1-measure.py
+++ Scripts/Class 7/7-1-measure.py
@@ -14,20 +14,66 @@ GPIO.setup(troyka, GPIO.OUT)
 GPIO.setup(comp, GPIO.IN)
 GPIO.setup(leds, GPIO.OUT)
 
+def decimal2binary(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
 
+def adc():
+    for i in range(256):
+        GPIO.output(dac, decimal2binary(i))
+      
+        time.sleep(0.001)
+        a = GPIO.input(comp)
+        if (a == 0):
+            break
+    GPIO.output(dac, 0)
+    GPIO.output(leds, decimal2binary(i))
+    y = i/255*3.3
+    return y            #возвращает значение напряжения на выходе тройки
 
+try:
+	measured_data = []
+	t_0 = datetime.now().time()
 
+	GPIO.output(troyka, 1)
+	i = 0
+	while (u <= 0.97*3.3):
+		u = adc()
+		measured_data[i] = u
+		i = i + 1
+		time.sleep(0.001)
 
+	GPIO.output(troyka, 0)
+	while (u >= 0.02*3.3):
+		u = adc()
+		measured_data[i] = u
+		i = i + 1
+		time.sleep(0.001)
 
+	t_1 = datetime.now().time()
+	delta_t = t_1 - t_0
 
+	step_quant = measured_data[2] - measured_data[1]
+	sr_v_diskr = len(measured_data) / delta_t
+	period = delta_t / len(measured_data)
 
+	plt.plot (measured_data)
+	plt.show()
 
-measured_data = [10, 23, 45, 50, 100, 155, 204, 255]
-plt.plot (measured_data)
-plt.show()
+	measured_data_str = [str(item) for item in measured_data]
+	step_quant_str = str(step_quant)
+	sr_v_diskr_str = str(sr_v_diskr)
 
-measured_data_str = [str(item) for item in measured_data]
-print (measured_data, measured_data_str)
+	with open("data.txt", "w") as outfile:
+		outfile.write ("\n".join (measured_data_str))
 
-with open("data.txt", "w") as outfile:
-	outfile.write ("\n".join (measured_data_str))
+	with open("settings.txt", "w") as outfile:
+		outfile.write ("\n".join (step_quant_str))
+		outfile.write ("\n".join (sr_v_diskr_str))
+
+	print("Продолжительность эксперимента: ", delta_t, ",период: ", period, ", частота:", sr_v_diskr)
+
+finally:
+    GPIO.output(dac, 0)
+    GPIO.output(troyka, 0)
+    GPIO.output(leds, 0)
+    GPIO.cleanup()
